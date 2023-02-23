a804cebc9837e9c0975c43f62171fa88e9322a8d
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-02-24 09:57:14 +1100

    fix date logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-"use client";
-
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const revalidate = 0;
+
 export default function Home() {
   const oneDay = 24 * 60 * 60 * 1000;
-  const originalIronDay = new Date(2023, 1, 22).getTime();
+  const todate = new Date(
+    new Date().toLocaleString("en", { timeZone: "Australia/Melbourne" })
+  );
+  const today = todate.setHours(0, 0, 0, 0);
+  const originalIronDay = new Date(
+    new Date(2023, 1, 23, 0, 0, 0, 0).toLocaleString("en", {
+      timeZone: "Australia/Melbourne",
+    })
+  ).getTime();
+  // const today = new Date().setHours(0, 0, 0, 0);
   const daysSinceOriginalIronDay = Math.round(
-    Math.abs((originalIronDay - Date.now()) / oneDay)
+    Math.abs((originalIronDay - today) / oneDay)
   );
   const isIronDay = daysSinceOriginalIronDay % 2 == 0;
 
